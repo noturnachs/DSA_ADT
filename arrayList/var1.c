f35e3a3c003582d7ebfbea093aaf9bb3951e8303
Lius <22102950@usc.edu.ph>
2025-08-27 21:41:55 +0800

    added insertSorted

--- arrayList/var1.c
+++ arrayList/var1.c
@@ -14,6 +14,7 @@ List initialize(List L);
 List insertPos(List L, int data, int position);
 List deletePos(List L, int position);
 int locate(List L, int data);
+List insertSorted(List L, int data);
 void display(List L);
 
 void main(){
@@ -22,20 +23,36 @@ void main(){
     L = initialize(L);
 	
     L = insertPos(L,1,0);
-    L = insertPos(L,2,1);
-    L = insertPos(L,3,2);
-	L = insertPos(L,4,3);
-	L = insertPos(L,5,4);
+    L = insertPos(L,7,1);
+    L = insertPos(L,67,2);
+	L = insertPos(L,90,3);
+	L = insertPos(L,200,4);
 
 //	L = insertPos(L,66,1);
 	
 //	L = deletePos(L, 3);
-	
+	insertSorted(L, 2);
     
     display(L);
-    printf("Finding Data 3, Index: %d", locate(L, 3));
+//    printf("Finding Data 3, Index: %d", locate(L, 3));
 
 
+}
+
+
+
+List insertSorted(List L, int data){
+	if(L.count < MAX){
+		int i;
+		for(i=L.count-1; i >= 0 && L.elem[i] > data; i--){
+			L.elem[i+1] = L.elem[i]; 
+		}
+		
+		L.elem[i+1] = data;
+		L.count++;
+	}
+	
+	return L;
 }
 
 int locate(List L, int data) {
